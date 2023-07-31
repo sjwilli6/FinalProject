9435084031d755653a0b92827147398ac7ffc1eb
Spencer Williams <sjwilli6@ncsu.edu>
2023-07-30 22:25:31 -0400

    Finished the about page

--- server.R
+++ server.R
@@ -7,17 +7,20 @@
 #    http://shiny.rstudio.com/
 #
 
+# Libraries Used
 library(shiny)
 library(randomForest)
 library(tree)
 library(caret)
+library(prediction)
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
   
-  # MLB Data
+  # Loading in MLB Data and columns I want to use
   mlb_data <- read.csv("/Users/monicabeingolea/Documents/ST558/mlb2019.csv")
   mlb_data <- mlb_data[ , c(2:3,9,11,13:31,36)]
+  mlb_data$pos1 <- as.factor(mlb_data$pos1)
   
 ### EDA Section ##################################################
   map_var_name = function(){
@@ -27,9 +30,9 @@ function(input, output, session) {
                   "Batting Avg"="avg",
                   "Stolen Bases"="SB"))
   }
-  
+  # Plot title
   output$expl_plot_title = renderUI(paste("Plot of", input$expl_variable))
-
+  # Creating scatter plots depending on variable selected
   exploratory_plot = function(){
     
     g <- ggplot(mlb_data, aes(x = AB))
@@ -51,20 +54,20 @@ function(input, output, session) {
         labs(x ="At Bats", y = "Stolen Bases", title = "Stolen Bases per AB")
     }
   }
-  
+  # Output plot
   output$expl_plot = renderPlot({exploratory_plot()})
-  
+  # Be able to download plot
   output$downloadPlot = downloadHandler(
     filename = function(){paste0(input$expl_variable, ".png")},
     content = function(file){ggsave(file, exploratory_plot(), device = "png")}
   )
-  
+  # Download data for specific variable name
   output$downloadData = downloadHandler(
     filename = function(){paste(map_var_name(),"data.csv")},
     content = function(file){write.csv(mlb_data1[,c("AB", "HR", map_var_name())],
                                        file, row.names = FALSE)}
   )
-  
+  # Output all of data
   output$downloadAllData = downloadHandler(
     filename = function(){"all_data.csv"},
     content = function(file){write.csv(mlb_data1, file)}
@@ -72,7 +75,7 @@ function(input, output, session) {
   
   #create text info
   output$info <- renderText({
-    
+    # Find the means for each variable selected 
     if(input$expl_variable == "Homeruns"){
       paste("The average MLB player in the 2019 season had", round(mean(mlb_data$HR, na.rm = TRUE), 2), "Homeruns.", sep = " ")
     } else if(input$expl_variable == "RBI's"){ 
@@ -86,7 +89,7 @@ function(input, output, session) {
     }
     
   })
-  
+  # Change the data table depending on what variable was selected
   stat_df = function(){
     if(input$expl_variable == "Homeruns"){
       mlb_data[ , c(1:5,10)]
@@ -100,12 +103,13 @@ function(input, output, session) {
       mlb_data[ , c(1:5,21)]
     }
   }
-  
+  # Output table
   output$statdf = renderDataTable({stat_df()})
   
 ### MathJAX ################################################### 
   
   output$mod1 <- renderUI({
+    # Using mathJAX to output multiple linear regression equation
     withMathJax(
       helpText('Y',tags$sub("i"), '=b',tags$sub("0"),'+b',tags$sub("1"),'x',tags$sub("1"),'+b',tags$sub("2"),'x',tags$sub("2"),'+...+b',tags$sub("p"),'x',tags$sub("p"))
     )
@@ -115,50 +119,87 @@ function(input, output, session) {
   
 ## Model Fitting ####################################################
 
-  set.seed(5432)
   # split data into test and training sets
   cut <- reactive({sample(1:nrow(mlb_data), input$proportion * nrow(mlb_data))})
-
+  # Create training and testing data sets
   mlb_train <- reactive({mlb_data[cut,]})
   mlb_test <- reactive({mlb_data[-cut,]})
   
+  # Creating data set based on the variables selected
+  df = function(){
+    mlb_train[cut,c(input$mod1_var, 11)]
+  }
+  df <- data.frame(df)
+  d = function(){
+    mlb_train[cut,c(input$mod2_var, 11)]
+  }
+  d <- data.frame(d)
+  y = function(){
+    mlb_train[cut,c(input$mod3_var, 11)]
+  }
+  y <- data.frame(y)
+  
+  # Fitting the multiple linear regression model
   fit_mod1 <- eventReactive(input$ready, {
     if(length(input$mod1_var) > 0) {
-      df = mlb_train[ ,c(input$mod1_var)]
-      df = df[complete.cases(df)]
-      lin_model <- lm(pos1 ~ ., data = df, preProcess = c("center", "scale"), trControl = trainControl(method = "cv", number = 5))
+      #df <- mlb_train[ ,c(input$mod1_var)]
+      #df <- df[complete.cases(df)]
+      lin_model <- lm(RBI ~ ., data = df, preProcess = c("center", "scale"), trControl = trainControl(method = "cv", number = 5))
       return(summary(lin_model))}
     else if (length(input$mod1_var) < 0) {
       (print("No Results"))}})
 
+    # Fitting the classification tree
   fit_mod2 <- eventReactive(input$ready, {
     if(length(input$mod2_var) > 0) {
-      d = mlb_train[ ,c(input$mod2_var)]
-      d = d[complete.cases(d),]
-      tree_model <- tree(pos1 ~ ., data = d)
+      #d <- mlb_train[ ,c(input$mod2_var)]
+      #d <- d[complete.cases(d),]
+      tree_model <- tree(RBI ~ ., data = d)
       return(plot(tree_model))}
   else if (length(input$mod2_var) < 0) {
     (print("No Results"))}})
   
+  # Fitting the random forest model
   fit_mod3 <- eventReactive(input$ready, {
     if(length(input$mod3_var) > 0) {
-      x = mlb_train[ ,c(input$mod3_var)]
-      x = x[complete.cases(x),]
-      y = mlb_train$pos1
-      rf <- randomForest(x = x, y = as.factor(y), mtry = ncol(mlb_train) - 1,
+      #x <- mlb_train[ ,c(input$mod3_var)]
+      #x <- x[complete.cases(x),]
+      y <- mlb_train$RBI
+      rf <- randomForest(x = x, y = y, mtry = ncol(mlb_train) - 1,
                            ntree = 200, importance = TRUE)
       return(rf$results, rf$bestTune)}
     else if (length(input$mod3_var) < 0) {
       (print("No Results"))}})
   
+  # Output the results
   output$fitted_mod1 = renderUI({fit_mod1()})
-  output$fitted_mod2 = renderUI({fit_mod2()})
+  output$fitted_mod2 = renderPlot({fit_mod2()})
   output$fitted_mod3 = renderUI({fit_mod3()})
+  
+  # statistics
+  #pred_1 <- predict(fit_mod1, newdata = mlb_test)
+  #stat_1 <- postResample(pred_1, obs = mlb_test$RBI)
+  #pred_2 <- predict(fit_mod2, newdata = mlb_test)
+  #stat_2 <- postResample(pred_2, obs = mlb_test$RBI)
+  #pred_3 <- predict(fit_mod3, newdata = mlb_test)
+  #stat_3 <- postResample(pred_3, obs = mlb_test$RBI)
+  
+  # Output the statistics
+  #output$compare_1 = renderUI({stat_1})
+  #output$compare_2 = renderUI({stat_2})
+  #output$compare_3 = renderUI({stat_3})
+  
+######################################################
+  
+## Prediction ####################################################
+  
+  
   
 ######################################################
   
 ## Data ####################################################
 
+    # Data table
   output$all_data = renderDataTable({mlb_data})
   
 ###################################################### 
--- ui.R
+++ ui.R
@@ -7,23 +7,24 @@ library(plotly)
 
 ### Info for the about section ###############################################################
 about_string = HTML("<h2>2019 MLB Season</h2>
-            <p>The Washington Nationals won the 2019 MLB World Series behind their star pitcher,
-            Stephen Strasburg. Data was gathered throughout the regular 2019 season and can be
-            found at <a href='https://baseballguru.com/bbdata1.html'>here</a>, along with past years.
-            Only the batting statistics will be used for analysis and visualizations.
+            <p>The Washington Nationals won the 2019 MLB World Series behind their star pitcher, Stephen Strasburg. Data was gathered throughout the regular 2019 season and can be found <a href='https://baseballguru.com/bbdata1.html'>here</a>, along with past years. Only the batting statistics will be used for analysis and visualizations.
             <br>
-            The purpose of this app is to take a deeper look into the 2019 MLB Season. A couple of questions
-            that come to mind are if certain positions have better hitting statistics or which teams performed
-            better in specific categories. The app is also going to look at different models that will help
-            predict certain qualities of a batter.
+            The purpose of this app is to take a deeper look into the 2019 MLB Season. Ultimately, we want to be able to predict the amount of RBI's a player is going to hit in a future season. This will all depend on the type of model and variables chosen by the user.
+            <br>
+            The app will have a total of six tabs. The EDA tab will contain different plots using at-bats (AB) as a variable. The user can determine the other variable and output will be created. The average during the 2019 season will be produced along with the correct variables in the data table. There will also be a modeling information page that will have details about the three different models I have chosen. Details include an overall definition, advantages, and disadvantages. The model fitting tab will allow the user to determine the variables used in the models. Statistics and plots will be outputted to help choose which model is best based on the testing set. The prediction tab will allow the user to select different values for the predictors to obtain a prediction for the response. The last tab will be the full data set that the user can scroll through and filter as needed.
+            <br>
             </p>")
-about_tab = tabItem("about",fluidRow(box(about_string, width = 8)))
+mlb_image <- img(src = 'mlbpicture.png', align = 'center', height = '300px', width = '400px')
+
+about_tab = tabItem("about",fluidRow(box(about_string, mlb_image, width = 8)))
 ####################################################################################################
 
 
 ### UI layout for the exploration tab ##############################################################
+# Choices for figure/data
 input_options = c("Homeruns", "RBI's", "Runs", "Batting Avg", "Stolen Bases")
 
+# Create an input for the user
 expl_controls = box(width = 4,
                     selectizeInput("expl_variable", "Select variable to examine: ", choices = input_options),
                     HTML("<br>"),
@@ -32,21 +33,25 @@ expl_controls = box(width = 4,
                     downloadButton("downloadData", label = "Download Current Data")
 )
 
+# Create the plot and data table based on the variable selected
 expl_plot = box(title = uiOutput("expl_plot_title"), width = 8,
                 plotOutput("expl_plot"),
                 HTML("<br>"),
                 textOutput("info"),
                 dataTableOutput("statdf"))
 
+# Output to tab
 exploration_tab = tabItem("exploration", fluidRow(expl_controls, expl_plot))
 ###########################################################
 
+# Model Paragraphs
 modInfo_string1 = HTML("<h2>Model Information</h2>
   <br>
   <h3>Multiple Linear Regression</h3>
   <p>A multiple linear regression model is used to determine a mathematical relationship among several random variables. It is fit by minimizing the sum of squared errors. Multiple linear regression models can include explanatory variables and higher order terms for predicting the dependent variable. Numerical and categorical data can also be included in these prediction models. Also, a big advantage of using these models is that it does a good job of predicting a response for a linear relationship.</p>
   <p>Many assumptions are made to fit a multiple linear regression model on data. If one of these assumptions are not met, the linear regression model will not be accurate. Some of the assumptions are that the variance of the residuals are constant, observations are independent, and multivariate normality. Multiple linear regression models can also be hard to interpret if there are complex variables and interaction terms.</p>
   ")
+# MathJAX
   uiOutput('mod1')
   
   modInfo_string2 = HTML("<h3>Classification Tree Analysis</h3>
@@ -57,50 +62,68 @@ modInfo_string1 = HTML("<h2>Model Information</h2>
   <p>A random forest model combines multiple decision trees to reach a single model for prediction. These models average across different trees to come to the final prediction model, often using bootstrapping. This type of model will less variance than a single tree. We can still look at the variable importance measured, but lose interpretability. Other advantages of using a random forest model is that we do not have to use all of the predictors. This model is preferred over bagged trees because it usually has less correlation due to strong predictors. One of the big limitations to a random forest model is that a large number of trees can make the algorithm too slow and ineffective in real time predictions. Random forest models are also more complicated to interpret than running a linear regression on data.</p>
   ")
   
+  # Output to tabs
 modInfo_tab = tabItem("model_info",fluidRow(box(modInfo_string1, uiOutput('mod1'), modInfo_string2, modInfo_string3, width = 8)))
 ####################################################################################################
 
 ### Model Fitting #################################################################
+# Breif explanation
 modelFit_string = HTML("<h2>Model Information</h2>
-                       <p>We will be fitting the three models to our data to predict the position variable. The models will be able to predict which position a player plays according to some of the hitting statistics recorded in the data set.</p>")
+                       <p>We will be fitting the three models to our data to predict the RBI variable. The models will be able to predict how many RBI's a player will have according to some of the hitting statistics recorded in the data set.</p>")
 
 
-
+# Select size for training/testing sets
 train_prop = box(width = 3, sliderInput("proportion", "Select Proportion for Training Data Set: ", min = 1, max = 100, value = 50, step = 1),)
 
+# Select the variables
 mod1_selection = box(width = 3,
                      checkboxGroupInput("mod1_var", "Variables to include in the Multiple Linear Regression Model: ",
-                                        c("G"="G", "AB"="AB", "R"="R", "H"="H", "db"="db", "tr"="tr", "HR"="HR", "RBI"="RBI", "SB"="SB",
-                                          "BB"="BB", "SO"="SO", "avg"="avg")))
-
+                                        names(mlb_data[c(4:10, 12, 14:15, 21)])))
+# Select the variables
 mod2_selection = box(width = 3,
                      checkboxGroupInput("mod2_var", "Variables to include in the Classification Tree Model: ",
-                                        c("G"="G", "AB"="AB", "R"="R", "H"="H", "db"="db", "tr"="tr", "HR"="HR", "RBI"="RBI", "SB"="SB",
-                                          "BB"="BB", "SO"="SO", "avg"="avg")))
-
+                                        names(mlb_data[c(4:10, 12, 14:15, 21)])))
+# Select the variables
 mod3_selection = box(width = 3,
                      checkboxGroupInput("mod3_var", "Variables to include in the Random Forest Model: ",
-                                        c("G"="G", "AB"="AB", "R"="R", "H"="H", "db"="db", "tr"="tr", "HR"="HR", "RBI"="RBI", "SB"="SB",
-                                          "BB"="BB", "SO"="SO", "avg"="avg")))
-
+                                        names(mlb_data[c(4:10, 12, 14:15, 21)])))
+# Action Button
 run_selection = actionButton("ready", "Run the Models")
 
+# Output the results
 model_output = box(uiOutput("fitted_mod1"),
                    plotOutput("fitted_mod2"),
                    uiOutput("fitted_mod3"))
 
+# Compare the models to the testing set
+#compare_output = box(uiOutput("stat_1"),
+ #                  uiOutput("stat_2"),
+  #                 uiOutput("stat_3"))
+
+# Output to tabs
 modFit_tab = tabItem("model_fit", fluidRow(modelFit_string, train_prop, mod1_selection, mod3_selection, mod3_selection, run_selection, model_output))
+####################################################################################################
+
+### Prediction ##############################################################
+pred_string = HTML("<h2>Prediction</h2>")
+
+# Output to tabs
+pred_tab = tabItem("prediction", fluidRow(pred_string))
 ####################################################################################################
 
 ### Data for Last Page ##############################################################
 all_data = HTML("<h2>2019 MLB Data</h2>",)
 
+# Create data table
 data_table <- dataTableOutput("all_data")
 
+# Output to tabs
 data_tab = tabItem("data", fluidRow(all_data, data_table))
 ####################################################################################################
 
+# Create dashboard with output tabs from above
 dashboardPage(
+  # Design pages
   title="MLB Dashboard",
   skin="red",
   dashboardHeader(title = "2019 MLB Season"),
@@ -118,7 +141,7 @@ dashboardPage(
   ),
   dashboardBody(
     tabItems(
-      about_tab, exploration_tab, modInfo_tab, modFit_tab, data_tab
+      about_tab, exploration_tab, modInfo_tab, modFit_tab, pred_tab, data_tab
     )
   )
 )
